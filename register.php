91893e0b9da2e6c61e6069042a8227b500fc2e9c
Sharick-Saragoza <ss.saragoza@student.alfa-college.nl>
2024-07-08 11:35:33 +0200

    New to do's

--- register.php
+++ register.php
@@ -10,59 +10,59 @@ include "incl/header.php";
 
 <!-- Main -->
         <div class="grid-item main">
-            <div class="register-card">
-            <div class="form-group">
-                <label class="data" for="firstname">Voornaam (Verplicht)</label>
-                <input class="data" type="text" id="firstname" placeholder="Voornaam..." />
-            </div>
-            <br>
-            <div class="form-group">
-                <label class="data" for="lasttname">Achternaam (Verplicht)</label>
-                <input class="data" type="text" id="lasttname" placeholder="Achternaam..." />
-            </div>
-            <br>
-            <div class="form-group">
-                <label class="data" for="infix">Tussenvoegsel </label>
-                <input class="data" type="text" id="infix" placeholder="Tussenvoegsel..." />
-            </div>
-            <br>
-            <div class="form-group">
-                <label class="data" for="address">Huisnummer (Verplicht)</label>
-                <input class="data" type="text" id="address" placeholder="00..." />
-            </div>
-            <br>
-            <div class="form-group">
-                <label class="data" for="street">Straatnaam (Verplicht)</label>
-                <input class="data" type="text" id="street" placeholder="Straat..." />
-            </div>
-            <br>
-            <div class="form-group">
-                <label class="data" for="addition">Toevoeging </label>
-                <input class="data" type="text" id="addition" placeholder="..." />
-            </div>
-            <br>
-            <div class="form-group">
-                <label class="data" for="postal code">Postcode (Verplicht)</label>
-                <input class="data" type="text" id="postal code" placeholder="0000 XX" />
-            </div>
-            <br>
-            <div class="form-group">
-                <label class="data" for="place">plaats (Verplicht)</label>
-                <input class="data" type="text" id="place" placeholder="plaats..." />
-            </div>
-            <br>
-            <div class="form-group">
-                <label class="data" for="email">email adress (Verplicht)</label>
-                <input class="data" type="text" id="email" placeholder="email..." />
-            </div>
-            <br>
-            <div class="form-group">
-                <label class="data" for="password">wachtwoord (Verplicht)</label>
-                <input class="data" type="text" id="password" placeholder="wachtwoord..." />
-            </div>
-            <br>
-            <button class="block" type="submit">Registreren</button>
-            </div>
+            <form method="POST" action="formhandlers/register.php" class="register-card">
+                <div class="form-group">
+                    <label class="data" for="firstname">Voornaam (Verplicht)</label>
+                    <input name="voornaam" class="data" type="text" id="firstname" placeholder="Voornaam..." />
+                </div>
+                <br>
+                <div class="form-group">
+                    <label class="data" for="lasttname">Achternaam (Verplicht)</label>
+                    <input name="achternaam" class="data" type="text" id="lasttname" placeholder="Achternaam..." />
+                </div>
+                <br>
+                <div class="form-group">
+                    <label class="data" for="infix">Tussenvoegsel </label>
+                    <input name="tussenvoegsel" class="data" type="text" id="infix" placeholder="Tussenvoegsel..." />
+                </div>
+                <br>
+                <div class="form-group">
+                    <label class="data" for="address">Huisnummer (Verplicht)</label>
+                    <input name="huisnummer" class="data" type="text" id="address" placeholder="00..." />
+                </div>
+                <br>
+                <div class="form-group">
+                    <label class="data" for="street">Straatnaam (Verplicht)</label>
+                    <input name="straatnaam" class="data" type="text" id="street" placeholder="Straat..." />
+                </div>
+                <br>
+                <div class="form-group">
+                    <label class="data" for="addition">Toevoeging </label>
+                    <input name="toevoeging" class="data" type="text" id="addition" placeholder="..." />
+                </div>
+                <br>
+                <div class="form-group">
+                    <label class="data" for="postal code">Postcode (Verplicht)</label>
+                    <input name="postcode" class="data" type="text" id="postal code" placeholder="0000 XX" />
+                </div>
+                <br>
+                <div class="form-group">
+                    <label class="data" for="place">plaats (Verplicht)</label>
+                    <input name="plaats" class="data" type="text" id="place" placeholder="plaats..." />
+                </div>
+                <br>
+                <div class="form-group">
+                    <label class="data" for="email">email adress (Verplicht)</label>
+                    <input name="email" class="data" type="email" id="email" placeholder="email..." />
+                </div>
+                <br>
+                <div class="form-group">
+                    <label class="data" for="password">wachtwoord (Verplicht)</label>
+                    <input name="wachtwoord" class="data" type="password" id="password" placeholder="wachtwoord..." />
+                </div>
+                <br>
+                <button class="block" type="submit">Registreren</button>
+            </form>
         </div>
 
 <!-- Include PHP footer -->
